b517ca7e36e42bf742022deba107bdeeaaf8033f
Matheo Chavez <chavmath@gmail.com>
2023-11-08 15:25:11 -0500

    Jugabilidad

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+   
+    [SerializeField]
+    Game gamePrefab;
+
+    public Game CurrentGame;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartGame(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (CurrentGame != null)
+        {
+            CheckForCurrentGameOver();
+        }
+	}
+
+    void CheckForCurrentGameOver()
+    {
+        if (CurrentGame.currLevelObj == null && CurrentGame.gameOver)
+        {
+            Destroy(CurrentGame.gameObject);
+            Debug.Log("Game Over!");
+        }
+    }
+    public void StartGame(int level)
+    {
+        CurrentGame = Instantiate(gamePrefab) as Game;
+        CurrentGame.gameObject.name = "Game";
+
+        CurrentGame.InitLevel(level, 3);
+    }
+}
